848aa968de69da2b9c53bc65369945bfa0de1727
MSG89 <111423362+MSG89@users.noreply.github.com>
2022-08-17 17:19:58 +0300

    added 4 directional movement

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -9,14 +9,25 @@ function gameLoop(state, game){
     const {wizard} = state;
     const {wizardElement} = game;
 
-    //move wizard
+    //move wizard R/L
     if (state.keys.KeyD){
-        state.wizard.posX += 2;
+        state.wizard.posX += wizard.speed;
+    }
+    if (state.keys.KeyA){
+        state.wizard.posX -= wizard.speed;
+    }
+    //move wizard U/D
+    if(state.keys.KeyW){
+        state.wizard.posY -= wizard.speed;
+    }
+    if(state.keys.KeyS){
+        state.wizard.posY += wizard.speed;
     }
 
     //Render
 
     wizardElement.style.left = wizard.posX + 'px';
-
+    wizardElement.style.top = wizard.posY + 'px';
+    
     window.requestAnimationFrame(gameLoop.bind(null, state, game));
 }
